4380824a3069eb314c0e49dd93881be20601d409
MetalArend <metalarend@gmail.com>
2018-06-28 00:13:16 +0200

    enhance the tests and variables used

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,12 +1,12 @@
-#!/bin/sh
+#!/usr/bin/env sh
 set -e
 
-if [ "${1:0:1}" = "-" ]; then
-  set -- /sbin/tini -- php /vendor/bin/phploc "$@"
-elif [ "$1" = "/vendor/bin/phploc" ]; then
+if [ "$(printf %c "$1")" = '-' ]; then
+  set -- /sbin/tini -- php /composer/vendor/bin/phploc "$@"
+elif [ "$1" = "/composer/vendor/bin/phploc" ]; then
   set -- /sbin/tini -- php "$@"
 elif [ "$1" = "phploc" ]; then
-  set -- /sbin/tini -- php /vendor/bin/"$@"
+  set -- /sbin/tini -- php /composer/vendor/bin/"$@"
 fi
 
 exec "$@"
